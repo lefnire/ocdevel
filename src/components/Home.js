6086311aa3eaa780740f649c3430391660dd9f68
Tyler Renelle <tylerrenelle@gmail.com>
2015-12-16 11:17:18 -0500

    hover-zones

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 export default class Home extends React.Component {
-	render(){
+
+	constructor(){
+		super();
+		this.state = {
+			hover: null
+		};
+		this.lorem = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+	}
+
+	render() {
 		return (
 			<div>
-				<h1 className="heading">The Portfolio Of</h1>
-
 				<div className='row front'>
-					<div className='col-md-6 lisa'>
-						<h2>Lisa Renelle</h2>
-						<p>Lisa was a nurse now a coder bla bla bla</p>
+					<div className='col-md-6 lisa' onMouseEnter={ ()=>this.setState({hover:'lisa'}) }>
+						{
+							this.state.hover === 'tyler' ?
+								<div className="personal-details">
+									<h2>Tyler Renelle</h2>
+									<p>{this.lorem}</p>
+								</div>
+								: null
+						}
 					</div>
-					<div className='col-md-6 tyler'>
-						<h2>Tyler Renelle</h2>
-						<p>Tyler made habitrpg bla bla bla</p>
+					<div className='col-md-6 tyler' onMouseEnter={ ()=>this.setState({hover:'tyler'}) }>
+						{
+							this.state.hover === 'lisa' ?
+								<div className="personal-details">
+									<h2>Lisa Renelle</h2>
+									<p>{this.lorem}</p>
+								</div>
+								: null
+						}
 					</div>
 				</div>
 			</div>
