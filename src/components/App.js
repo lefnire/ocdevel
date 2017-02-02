3fcd5e239a6cff53e30ab081a75a9b7e74a76613
Tyler Renelle <tylerrenelle@gmail.com>
2017-02-02 15:20:44 -0700

    fixes & optimizations

--- src/components/App.js
+++ src/components/App.js
@@ -14,9 +14,10 @@ export default class App extends React.Component {
             <img className='head-image' src={images.cathead} />
             <p className="Dictionary img-rounded">ocdevel; [O-C-Devel]<br/>
               (noun)<br/>
-              1.orange cat development <br/> 2. developed by tyler renelle in 2009. <br/>
+              1. orange cat development <br/>
+              2. created by tyler renelle in 2007.<br/>
               3. web & mobile app development by a married duo with a profound love for programming.<br/>
-              4. specializing in javascript, react, ionic, angular, python, and node.</p>
+              4. specializing in javascript, react, python, and node.</p>
           </Jumbotron>
 
         <Parallax bgImage={images.bird} strength={400} log={true}>
@@ -33,10 +34,10 @@ export default class App extends React.Component {
         <Jumbotron title="Tyler Renelle" />
         <img className='head-image' src={images.tylerHead} />
         <ul className="social-buttons">
-          <li><i href="https://www.facebook.com/lefnire" className="zocial facebook icon">Facebook</i></li>
-          <li><i href="https://www.linkedin.com/in/lefnire" className="zocial linkedin icon">LinkedIn</i></li>
-          <li><i href="https://twitter.com/lefnire" className="zocial twitter icon">Twitter</i></li>
-          <li><i href="https://github.com/lefnire" className="zocial github icon">Github</i></li>
+          <li><a target="_blank" href="https://www.facebook.com/lefnire" className="zocial facebook icon">Facebook</a></li>
+          <li><a target="_blank" href="https://www.linkedin.com/in/lefnire" className="zocial linkedin icon">LinkedIn</a></li>
+          <li><a target="_blank" href="https://twitter.com/lefnire" className="zocial twitter icon">Twitter</a></li>
+          <li><a target="_blank" href="https://github.com/lefnire" className="zocial github icon">Github</a></li>
         </ul>
         </Parallax>
         <p className="description">Tyler is a full-stack Senior JavaScript developer and has spent 10 years in web & mobile. He is focused on Node,
@@ -50,10 +51,10 @@ export default class App extends React.Component {
           <Jumbotron title="Lisa Renelle" />
           <img className='head-image' src={images.lisaHead} />
           <ul className="social-buttons">
-            <li><i target='_blank' href="https://www.facebook.com/lisa.haskellbunker" className="zocial facebook icon"/></li>
-            <li><i target='_blank' href="https://www.linkedin.com/in/lisa-renelle-243413106" className="zocial linkedin icon"/></li>
-            <li><i target='_blank' href="https://github.com/LisaMarie7073" className="zocial github icon"/></li>
-            <li><i target='_blank' href="https://www.pinterest.com/lillisamhaskell/" className="zocial pinterest icon"/></li>
+            <li><a target='_blank' href="https://www.facebook.com/lisa.haskellbunker" className="zocial facebook icon"/></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/lisa-renelle-243413106" className="zocial linkedin icon"/></li>
+            <li><a target='_blank' href="https://github.com/LisaMarie7073" className="zocial github icon"/></li>
+            <li><a target='_blank' href="https://www.pinterest.com/lillisamhaskell/" className="zocial pinterest icon"/></li>
           </ul>
         </Parallax>
           <p className="description">Lisa is a front-end, Junior JavaScript developer and been in the working with javascript for almost a year. She has currently been
@@ -64,7 +65,7 @@ export default class App extends React.Component {
           <div>
             <Contact />
             <p className="kari">Photography by the talented <a href="https://www.kariannphotography.com">Kari Ann Haskell.</a>
-              <br/> Also, thank you <a href="https://www.myfreetextures.com">myfreetextures.com.</a></p>
+            <br/> Also, thank you <a href="https://www.myfreetextures.com">myfreetextures.com.</a></p>
           </div>
         </Parallax>
 
@@ -74,10 +75,10 @@ export default class App extends React.Component {
 }
 
 let images = {
-  bird: require('../assets/moss.jpg'),
+  bird: require('../assets/moss-low.jpg'),
   bgOCD: require('../assets/Background-OCD.jpg'),
   bgImage: require('../assets/water.png.jpg'),
-  oregon: require('../assets/oregon.jpg'),
+  oregon: require('../assets/oregon-low.jpg'),
   stix2: require('../assets/stix2.png'),
   lisaHead: require('../assets/Lisa/lisahead2.png'),
   tylerHead: require('../assets/Tyler/tylerhead.png'),
