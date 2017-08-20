bdd9d8a476069fb4e666f8fb753d9ea581262d64
Tyler Renelle <tylerrenelle@gmail.com>
2017-08-20 16:20:34 -0700

    episode 23

--- src/content/machine-learning.js
+++ src/content/machine-learning.js
@@ -876,6 +876,42 @@ RNNs
   - Linear algebra stuff (PCA, LSA, SVD)
   - Pros (?): faster, more accurate, incremental fitting. Cons (?): data hungry, more RAM. [More info](http://blog.aylien.com/overview-word-embeddings-history-word2vec-cbow-glove/)
 - DNN for POS, NER (or RNNs)`
+}, {
+  title: '23. Deep NLP 2',
+  date: "2017-08-20",
+  guid: "1346120e3e578b15c8f34b31bc21ef78",
+  file: {},
+  libsynEpisode: 5660423,
+  teaser: 'RNN review, bi-directional RNNs, LSTM & GRU cells.',
+  body: `## Resources
+- ${resources.other.rnn_articles}
+- ${resources.courses.cs224n}
+- ${resources.other.tf_tuts_rnns}
+- The usual DL resources (pick one):
+  - ${resources.books.dl_book}
+  - ${resources.courses.fastai}
+  - ${resources.other.nns_and_dl}
+
+
+## Episode
+
+RNN Review
+  - Vanilla: When words + running context is sufficient. 
+    - POS, NER, stocks, weather
+  - Bidirectional RNN (BiLSTM): When stuff from right helps too
+  - Encoder/decoder or Seq2seq: When you should hear everything first / spin a different way
+    - Classification, sentiment, translation
+  - Now w/ word embeddings
+
+Train: backprop through time
+  - Vanishing/exploding gradient
+
+[LSTMs](http://colah.github.io/posts/2015-08-Understanding-LSTMs/)
+  - ReLU vs Sigmoid vs TanH (Nonlinearities future episode)
+  - Forget gate layer
+  - Input gate layer: decides which values to update
+  - Tanh layer: creates new candidate values
+  - Output layer`
 }];
 
 const podcast = {
