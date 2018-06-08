bb1aab1773d44dce8e13b36e8c9fe10b62363cee
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-06-07 17:29:00 -0700

    A rough attempt a fixing it.

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+
+const Header = () => {
+  return (
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Machine Learning Podcast</a>
+      <a href="#">Contact</a>
+    </nav>
+
+  )
+}
+
+export default Header;
