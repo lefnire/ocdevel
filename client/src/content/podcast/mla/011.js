c6c3b2473b146bcbccfdb83eaf14921aa13b517f
Tyler Renelle <tylerrenelle@gmail.com>
2021-01-05 09:43:22 -0800

    mla-11 show-notes

--- client/src/content/podcast/mla/011.js
+++ client/src/content/podcast/mla/011.js
@@ -4,5 +4,13 @@ export default {
   mla: true,
   date: "2020-11-07",
   guid: "43675078",
-  teaser: "Kmeans (sklearn vs FAISS), finding n_clusters via inertia/silhouette, Agglomorative, DBSCAN/HDBSCAN"
+  teaser: "Kmeans (sklearn vs FAISS), finding n_clusters via inertia/silhouette, Agglomorative, DBSCAN/HDBSCAN",
+  body: `
+Clustering via Kmeans, Agglomorative, or DBSCAN/HDBSCAN.
+
+* Kmeans. [sklearn.cluster.KMeans](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html), [Faiss.kmeans](https://github.com/facebookresearch/faiss/wiki/Faiss-building-blocks:-clustering,-PCA,-quantization). 
+* Finding optimal number of clusters n_clusters. [kmeans.inertia_](https://scikit-learn.org/stable/modules/clustering.html#k-means) inside a loop, pass results to [kneed.KneeLocator](https://kneed.readthedocs.io/en/stable/) (Gnothi [sample code](https://github.com/lefnire/ml-tools/blob/master/ml_tools/similars.py)). Or [silhouette_score](https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html)  (preferred).
+* [Agglomorative clustering](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html) with pre-computed cosine similarity square matrix (Gnothi [sample code](https://github.com/lefnire/ml-tools/blob/master/ml_tools/similars.py))
+* [DBSCAN](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html) / [HDBSCAN](https://hdbscan.readthedocs.io/en/latest/index.html) 
+`
 }
