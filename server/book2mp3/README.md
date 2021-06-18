54ec12bf8f1dce902cdfdb01ce4823b7e551e60b
Tyler Renelle <tylerrenelle@gmail.com>
2021-06-18 16:45:15 -0700

    WIP

--- server/book2mp3/README.md
+++ server/book2mp3/README.md
@@ -1,1 +1,3 @@
 Use TensorflowTTS or find a better state of the art TTS solution. Polly has a max-characters restriction of 1500 words, so I'd need to split into multiple jobs (not worth it). Also relatively expensive, especially if I'm experimenting.
+
+mozilla/tts is better than tensorflowtts. But Zamar online service (MS Sam) is still better.
--- server/book2mp3/txt2tensorflowtts.py
+++ server/book2mp3/txt2tensorflowtts.py
@@ -3,51 +3,36 @@ pip install tensorflow tensorflowtts soundfile
 pip install git+https://github.com/repodiac/german_transliterate
 """
 
-from pathlib import Path
-import numpy as np
-import soundfile as sf
-import yaml
-import tensorflow as tf
-from tensorflow_tts.inference import TFAutoModel
-from tensorflow_tts.inference import AutoProcessor
-
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("-i", required=True)
-# args = parser.parse_args()
-
-# initialize fastspeech2 model.
-fastspeech2 = TFAutoModel.from_pretrained("tensorspeech/tts-fastspeech2-ljspeech-en")
-
-
-# initialize mb_melgan model
-mb_melgan = TFAutoModel.from_pretrained("tensorspeech/tts-mb_melgan-ljspeech-en")
-
-
-# inference
-processor = AutoProcessor.from_pretrained("tensorspeech/tts-fastspeech2-ljspeech-en")
 
 in_ = 'frogs.txt'
 # in_ = str(Path(args.i).expanduser())
 with open(in_) as f:
     lines = f.readlines()
-txt = '\n'.join(lines)
+text = '\n'.join(lines)
 
-input_ids = processor.text_to_sequence(txt)
-# fastspeech inference
+import soundfile as sf
+import numpy as np
 
-mel_before, mel_after, duration_outputs, _, _ = fastspeech2.inference(
+import tensorflow as tf
+
+from tensorflow_tts.inference import AutoProcessor
+from tensorflow_tts.inference import TFAutoModel
+
+processor = AutoProcessor.from_pretrained("tensorspeech/tts-tacotron2-ljspeech-en")
+tacotron2 = TFAutoModel.from_pretrained("tensorspeech/tts-tacotron2-ljspeech-en")
+mb_melgan = TFAutoModel.from_pretrained("tensorspeech/tts-mb_melgan-ljspeech-en")
+
+input_ids = processor.text_to_sequence(text[:100000])
+
+# tacotron2 inference (text-to-mel)
+decoder_output, mel_outputs, stop_token_prediction, alignment_history = tacotron2.inference(
     input_ids=tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+    input_lengths=tf.convert_to_tensor([len(input_ids)], tf.int32),
     speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
-    speed_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
-    f0_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
-    energy_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
 )
 
-# melgan inference
-audio_before = mb_melgan.inference(mel_before)[0, :, 0]
-audio_after = mb_melgan.inference(mel_after)[0, :, 0]
+# melgan inference (mel-to-wav)
+audio = mb_melgan.inference(mel_outputs)[0, :, 0]
 
 # save to file
-sf.write(f'{in_}.before.wav', audio_before, 22050, "PCM_16")
-sf.write(f'{in_}.after.wav', audio_after, 22050, "PCM_16")
+sf.write('./audio.wav', audio, 22050, "PCM_16")
