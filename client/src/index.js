e7f2969a8a6eaa200395ab083156626a8da78c71
Tyler Renelle <tylerrenelle@gmail.com>
2021-11-01 14:19:22 -0700

    conditional GA

--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,27 @@ import React, {useEffect} from 'react'
 import ReactDom from 'react-dom'
 import {BrowserRouter as Router, useHistory} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'css-social-buttons/css/zocial.css'
 
 import App from './components/App'
 
-import ReactGA from 'react-ga'
-// ReactGA.initialize('UA-3128634-8')
-// ReactGA.pageview(window.location.pathname)
+let usingGA = false;
+if (process.env.NODE_ENV === "production") {
+  console.log("using GA")
+  usingGA = true;
+  ReactGA.initialize('UA-3128634-8')
+  ReactGA.pageview(window.location.pathname)
+}
 
 function Index() {
   const {listen} = useHistory()
 
   // https://medium.com/javascript-in-plain-english/google-analytics-with-react-router-and-hooks-16d403ddc528
   useEffect(() => {
+    if (!usingGA) {return}
     return listen((location) => {
       ReactGA.pageview(location.pathname) //  + location.search);
       window.scrollTo(0, 0);
