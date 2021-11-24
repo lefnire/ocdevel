9693d2b09152517df8f9770c4587df513f632490
Tyler Renelle <tylerrenelle@gmail.com>
2021-11-24 10:13:15 -0800

    mlg2 update

--- client/src/content/podcast/mlg/002/index.js
+++ client/src/content/podcast/mlg/002/index.js
@@ -8,9 +8,10 @@ const ep = {
   episode: 2,
   mergeEpisode: 2,
   created: "2017-02-09",
+  updated: "2021-11-23",
   guid: "129d0157-fbda-4cc6-aaae-1c96745c12c9",
-  libsynEpisode: 5440757,
-  teaser: "What is artificial intelligence and machine learning? What's the difference? How about compared to statistics and data science? AI history.",
+  libsynEpisode: 21268487,
+  teaser: "What is artificial intelligence, machine learning, and data science? What are their differences? AI history.",
   body: Body,
   transcript: Transcript
 }
