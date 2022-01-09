fa58d8526cc85a37a1fbad37e9be3072a1104798
Tyler Renelle <tylerrenelle@gmail.com>
2022-01-09 14:41:24 -0800

    single dev server. vpc-peering to gnothi

--- terraform/general/main.tf
+++ terraform/general/main.tf
@@ -22,16 +22,10 @@ provider "aws" {
   skip_requesting_account_id = false
 }
 
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com"
-}
-
 locals {
   name = "ocdevel-general"
   region = "us-east-1"
   main_az = "us-east-1a"
-  mount_path = "/home/ec2-user/efs"
-  myip = ["${chomp(data.http.myip.body)}/32"]
   tags = {
     app = local.name
     Name = local.name
@@ -59,105 +53,3 @@ module "vpc" {
   tags = local.tags
 }
 
-resource "aws_security_group" "sg_ec2" {
-  name        = "${local.name}-sg-ec2"
-  description = "${local.name} dev SG (ssh)"
-  vpc_id      = module.vpc.vpc_id
-  tags = local.tags
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = local.myip
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_security_group" "sg_efs" {
-  name        = "${local.name}-sg-efs"
-  description = "${local.name} dev SG (efs)"
-  vpc_id      = module.vpc.vpc_id
-  tags = local.tags
-
-  ingress {
-    description = "EFS mount target"
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    security_groups = [aws_security_group.sg_ec2.id]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_efs_file_system" "efs" {
-  creation_token = "${local.name}-efs"
-  encrypted = true
-  tags = local.tags
-}
-
-resource "aws_efs_mount_target" "mount" {
-  file_system_id = aws_efs_file_system.efs.id
-  subnet_id      = element(module.vpc.private_subnets, 0)  
-  security_groups = [aws_security_group.sg_efs.id]
-}
-
-locals {
-  # As local so I can print the commands as output, to debug via SSH.
-  # Using Amazon Linux 2 AMI (yum) since it was easier to get working with amazon-efs-utils / nfs-utils than Ubuntu
-  user_data = <<EOF
-#!/bin/bash
-yum install amazon-efs-utils -y
-mkdir -p ${local.mount_path}
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs.dns_name}:/ ${local.mount_path}
-echo ${aws_efs_file_system.efs.dns_name}:/ ${local.mount_path} nfs4 defaults,_netdev 0 0  | cat >> /etc/fstab
-chmod go+rw ${local.mount_path}
-
-yum update -y && yum upgrade -y && reboot now
-EOF
-}
-
-# EFS setup: https://github.com/Apeksh742/EFS_with_terraform/blob/master/main.tf
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = local.name
-
-  ami                    = "ami-0ed9277fb7eb570c9"
-  instance_type          = "t2.medium"
-  key_name = "aws-general"
-  availability_zone           = local.main_az
-  subnet_id                   = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids      = [aws_security_group.sg_ec2.id]
-  associate_public_ip_address = true
-
-  tags = local.tags
-
-  user_data = local.user_data
-}
-
-resource "aws_eip" "eip" {
-  instance = module.ec2_instance.id
-  vpc      = true
-}
-
-output "ec2_ip" {
-  value = "ssh -i ~/.ssh/aws-general.pem ec2-user@${aws_eip.eip.public_ip}"
-}
-
-output "efs_dns" {
-  value = local.user_data
-}
