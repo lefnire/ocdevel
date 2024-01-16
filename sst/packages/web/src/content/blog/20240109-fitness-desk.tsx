053050e3bcac3b4044177d60119aa7e4220bd1bc
Tyler Renelle <tylerrenelle@gmail.com>
2024-01-15 20:18:23 -0800

    prettier tags & text. note switching from flex to float, due to tags-list size - fix this

--- sst/packages/web/src/content/blog/20240109-fitness-desk.tsx
+++ sst/packages/web/src/content/blog/20240109-fitness-desk.tsx
@@ -30,18 +30,18 @@ const useStore = create<UseStore>()(immer((set, get) => ({
 })))
 
 function Tag({name}: {name: string}) {
+  const active = useStore(store => store.tags?.[name])
   const clickFilter = () => {
     useStore.getState().toggleTag(name)
   }
 
   if (name === 'c') {return null}
-  return <Badge
-    className='me-1'
-    bg="secondary pointer"
+  return <span
+    className={`me-1 text-muted pointer text-decoration-underline ${active ? "bg-info" : ""}`}
     onClick={clickFilter}
   >
-    {name}
-  </Badge>
+    #{name}
+  </span>
 }
 
 interface Node {
@@ -60,6 +60,7 @@ function Node({id, text, note, tags, children, depth, passesFilter}: Node) {
   const hasChildren = children?.length > 0
   let show = true
   if (anyTags) {
+    // FIXME filters are OR, not AND. Should be AND
     if (!passesFilter) {
       show = Object.keys(tags).some(key => tags[key] === true && appliedTags[key] === true);
     }
@@ -81,19 +82,21 @@ function Node({id, text, note, tags, children, depth, passesFilter}: Node) {
     className="ps-2"
   >
     <div
-      className={`gap-2 d-flex flex-row ${show ? "" : "visually-hidden"}`}
+      className={`${show ? "" : "visually-hidden"}`}
     >
       <div
-        className="pointer"
+        className="pointer me-2 float-start"
         onClick={toggle}
       >{chevron}</div>
-      <div className='' dangerouslySetInnerHTML={{__html: text}} />
+      <div className='float-start me-2' dangerouslySetInnerHTML={{__html: text}} />
       <div className=''>
         {Object.keys(tags).map(k => <Tag name={k} />)}
       </div>
     </div>
+    <div className='clearfix'></div>
     <div
-      className={`text-muted lh-sm fw-light ms-1 ${show ? "" : "visually-hidden"}`}
+      className={`text-muted lh-sm ms-1 ${show ? "" : "visually-hidden"}`}
+      style={{fontSize: '0.85rem'}}
       dangerouslySetInnerHTML={{__html: note}}
     />
     <div
@@ -121,10 +124,11 @@ function AppliedTags() {
   const appliedTags = Object.entries(tags).filter(([k, v]) => v).map(([k, v]) => (
     <Tag name={k} />
   ))
-  return <div>
-    <div>Applied tags (click to remove)</div>
-    {appliedTags}
-    <hr/>
+  return <div className="card">
+    <div className="card-body">
+      <h6>Applied tags (click to remove)</h6>
+      {appliedTags}
+    </div>
   </div>
 }
 
