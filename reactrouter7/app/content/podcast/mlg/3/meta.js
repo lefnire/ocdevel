489050e326d536e2c10ca8a797408ab8982f22f1
Tyler Renelle <tylerrenelle@gmail.com>
2025-03-27 17:06:50 -0600

    mlg3 transcript & teaser/body

--- reactrouter7/app/content/podcast/mlg/3/meta.js
+++ reactrouter7/app/content/podcast/mlg/3/meta.js
@@ -4,4 +4,4 @@ export const mergeEpisode = 3
 export const created = "2017-02-10"
 export const guid = "a0b24583-e253-492c-addc-ee0c0aeb1765"
 export const libsynEpisode = 5440756
-export const teaser = "Why should you care about AI? Inspirational topics about economic revolution, the singularity, consciousness, and fear."
+export const teaser = "Why I believe AI is the most important thing happening in the world right now. I walk through four major philosophical conversations around AI: economic automation, the singularity, consciousness, and the risks of misaligned goals."
