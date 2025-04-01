558a45f25409cdde369b3ae815cb320560df75c4
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-01 09:38:28 -0600

    mlg/8 transcript

--- reactrouter7/app/content/podcast/mlg/8/meta.js
+++ reactrouter7/app/content/podcast/mlg/8/meta.js
@@ -4,7 +4,7 @@ export const mergeEpisode = 8
 export const created = "2017-02-23"
 export const guid = "a5c01d38-5242-4b63-b265-81fc53d38ad3"
 export const libsynEpisode = 5440751
-export const teaser = "Introduction to the branches of mathematics used in machine learning. Linear algebra, statistics, calculus."
+export const teaser = "Mathematical foundations necessary for successful machine learning, including linear algebra, statistics, and calculus. I encourage learning machine learning first, then tackling the math later - this can enhance understanding and retention."
 export const file = {
   url: "http://ocdevel.com/files/podcasts/machine-learning/ml-8.mp3",
   length: 24852040,
