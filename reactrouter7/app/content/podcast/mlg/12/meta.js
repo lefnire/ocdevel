eaa5957759997eb45179046d97676a8d483a554c
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-01 10:51:26 -0600

    mlg/12 transcript

--- reactrouter7/app/content/podcast/mlg/12/meta.js
+++ reactrouter7/app/content/podcast/mlg/12/meta.js
@@ -4,7 +4,7 @@
   export const created = "2017-03-19"
   export const guid = "1074a375-6831-456d-9bbc-d28c8f85a557"
   export const libsynEpisode = 5440746
-  export const teaser = "Speed-run of some shallow algorithms: K Nearest Neighbors (KNN); K-means; Apriori; PCA; Decision Trees"
+  export const teaser = "Shallow learning algorithms including K Nearest Neighbors, K Means, and decision trees. Supervised, unsupervised, and reinforcement learning methods for practical machine learning applications."
   export const file = {
     url: "http://ocdevel.com/files/podcasts/machine-learning/ml-12.mp3",
     length: 50030574,
