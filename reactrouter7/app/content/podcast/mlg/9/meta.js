07d90247e294d33cff26433ed166c24cd2be12f6
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-01 10:34:01 -0600

    mlg/9 transcript

--- reactrouter7/app/content/podcast/mlg/9/meta.js
+++ reactrouter7/app/content/podcast/mlg/9/meta.js
@@ -4,7 +4,7 @@ export const mergeEpisode = 9
 export const created = "2017-03-04"
 export const guid = "d842fe61-7cf2-4209-9cb3-d29be6c4d1a8"
 export const libsynEpisode = 5440749
-export const teaser = "Deep learning and neural networks. How to stack our logisitic regression units into a multi-layer perceptron."
+export const teaser = "Deep learning and artificial neural networks are the driving forces behind the latest advancements in artificial intelligence across various domains. Explore neural networks, supervised learning's subspace, and how deep learning models like convolutional and recurrent neural networks are revolutionizing fields such as vision and language processing."
 export const file = {
   url: "http://ocdevel.com/files/podcasts/machine-learning/ml-9.mp3",
   length: 45855231,
