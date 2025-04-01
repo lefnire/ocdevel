4e5ca57dec043b176a1373bf71671c584b9f7428
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-01 10:39:34 -0600

    mlg/10 transcript

--- reactrouter7/app/content/podcast/mlg/10/meta.js
+++ reactrouter7/app/content/podcast/mlg/10/meta.js
@@ -4,7 +4,7 @@ export const mergeEpisode = 10
 export const created = "2017-03-07"
 export const guid = "c613d746-0916-448e-8315-5ac4323389e2"
 export const libsynEpisode = 5440748
-export const teaser = "Languages & frameworks comparison. Languages: Python, R, MATLAB/Octave, Julia, Java/Scala, C/C++. Frameworks: Hadoop/Spark, Deeplearning4J, Theano, Torch, TensorFlow."
+export const teaser = "Python and PyTorch / TensorFlow rise as top choices for machine learning due to performance enhancements in computational graph frameworks, making them recommended for both budding and experienced ML engineers. Traditional languages like C++ and specialized math languages such as R and MATLAB each have specific use cases but are overshadowed by Python's all-encompassing capabilities supported by a rich ecosystem of libraries."
 export const file = {
   url: "http://ocdevel.com/files/podcasts/machine-learning/ml-10.mp3",
   length: 39407399,
