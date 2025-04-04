e735819b5efc9d9cb61d7c773605b7df73c396c2
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-03 22:31:33 -0600

    mlg/16 transcript

--- reactrouter7/app/content/podcast/mlg/16/meta.js
+++ reactrouter7/app/content/podcast/mlg/16/meta.js
@@ -4,7 +4,7 @@
   export const created = "2017-05-21"
   export const guid = "c2db5df8-936b-4404-8f0f-7eb188bfe9ab"
   export const libsynEpisode = 5440742
-  export const teaser = "Can AI be conscious?"
+  export const teaser = "Explores the controversial topic of artificial consciousness, discussing the potential for AI to achieve consciousness and the implications of such a development. Definitions and components of consciousness, the singularity, and various theories related to the capability of AI to be conscious, considering perspectives like emergence, functionalism, and biological plausibility."
   export const file = {
     url: "http://ocdevel.com/files/podcasts/machine-learning/ml-16.mp3",
     length: 69807705,
