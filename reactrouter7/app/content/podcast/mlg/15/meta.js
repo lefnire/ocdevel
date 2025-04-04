c01aff87bcdcb544b04756af78e5e227e25e1ba6
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-03 22:24:00 -0600

    mlg/15 transcript

--- reactrouter7/app/content/podcast/mlg/15/meta.js
+++ reactrouter7/app/content/podcast/mlg/15/meta.js
@@ -4,7 +4,7 @@
   export const created = "2017-05-07"
   export const guid = "7da253aa-b035-4702-8475-55b8d3eeeebd"
   export const libsynEpisode = 5440743
-  export const teaser = "Performance evaluation & improvement"
+  export const teaser = "Deep dive into performance evaluation and improvement in machine learning. Critical concepts like bias, variance, accuracy, and the role of regularization in curbing overfitting and underfitting."
   export const file = {
     url: "http://ocdevel.com/files/podcasts/machine-learning/ml-15.mp3",
     length: 37982381 ,
