a02c7dce4af69c50034bdd153e04de65744ce5c5
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-05 15:32:31 -0600

    sperax

--- reactrouter7/app/content/product-links/treadmills/index.tsx
+++ reactrouter7/app/content/product-links/treadmills/index.tsx
@@ -5,7 +5,8 @@ import sekoday_lube from './sekoday_lube'
 import desk from './flexispot_en1'
 import premium from './lifespan_tx6'
 import value from './urevo_cyberpad'
-import budget from './deerrun_q1mini'
+// import budget from './deerrun_q1mini'
+import budget from './sperax_motioneaselitep1'
 
 export type AffiliateLink_ = AffiliateLink & {linkText: string, topTitle?: string}
 export const treadmills: AffiliateLink_[] = [
