42c02dd102af489098f2b87e1e5a2a02f3320a3d
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-06 15:03:05 -0600

    mlg/18 transcript

--- reactrouter7/app/content/podcast/mlg/18/meta.js
+++ reactrouter7/app/content/podcast/mlg/18/meta.js
@@ -5,4 +5,4 @@ export const created = "2017-06-25"
 export const guid = "d8ebdbe6640d0d34f12778f90b91db8d"
 export const file = {}
 export const libsynEpisode = 5479957
-export const teaser = 'Introduction to Natural Language Processing (NLP) topics.'
+export const teaser = 'Introduces the subfield of machine learning called Natural Language Processing (NLP), exploring its role as a specialization that focuses on understanding human language through computation. NLP involves transforming text into mathematical representations and includes applications like machine translation, chatbots, sentiment analysis, and more.'
