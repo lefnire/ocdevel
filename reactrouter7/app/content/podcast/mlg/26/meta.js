39a89ad0a3d50f6219dd25e2ea4b698c7314fa76
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-09 08:14:52 -0600

    mlg/26 transcript

--- reactrouter7/app/content/podcast/mlg/26/meta.js
+++ reactrouter7/app/content/podcast/mlg/26/meta.js
@@ -5,4 +5,4 @@
   export const guid = "e704eb47d4280a7abc9bb6f0895a7b26"
   export const file = {}
   export const libsynEpisode = 6194090
-  export const teaser = 'Community project & intro to Bitcoin/crypto + trading'
+  export const teaser = 'Community project: A Bitcoin trading bot to sharpen your machine learning skills. The project uses crypto trading to explore machine learning concepts like hyperparameter selection and deep reinforcement learning, candlesticks, price actions, and various ML techniques.'
--- reactrouter7/app/routes/walk/meta.js
+++ reactrouter7/app/routes/walk/meta.js
@@ -4,7 +4,7 @@ export const date = '2024-01-09'
 export const updated = '2025-03-22'
 export const title = "Walking Desks"
 export const head = {
-  title: "Best Walking Pad in 2025",
+  title: "Best Walking Pads 2025",
   description: "Compares walking pad brands for a walking desk / treadmill desk. Recommends standing desks, monitor arms, and ergonomic peripherals. Walking pad comparison includes [placeholder], and more."
 }
 export const pinned = true
