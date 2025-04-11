5c1452007919d099468f2539528c5777e2218bc9
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-11 11:37:59 -0600

    fix

--- reactrouter7/app/routes/mlg.resources/route.tsx
+++ reactrouter7/app/routes/mlg.resources/route.tsx
@@ -17,13 +17,16 @@ export async function loader(props: Route.LoaderArgs) {
   }
 }
 
+const about = "Best machine learning resources. A curated syllabus of learning resources for machine learning, artificial intelligence, and data science."
+
 export default function Resources({loaderData}: Route.ComponentProps) {
   return <div className="podcasts">
     <Navbar />
     <Container fluid className="resources">
-      <div className='mb-3 mlg-update ps-3 small'>
-        [Updated 2025-04-08] These are resources to learn machine learning & data science. The resources are in tree-structure, in descending order of value. Use the Filters on the left to narrow your search. Hover over each button for more help. To suggest a resource, or discuss/contend resources listed here, comment <a href="https://github.com/lefnire/ocdevel/issues/43" target="_blank">here</a>. <Link to="/walk">Try a walking desk</Link> to stay healthy while you study or work!
-      </div>
+      <p className='mlg-update ps-3 small'>
+        {about} Organized in tree-structure, in descending order of value. Use the Filters on the left to narrow your search. Hover over each button for more help. {/*To suggest a resource, or discuss/contend resources listed here, comment <a href="https://github.com/lefnire/ocdevel/issues/43" target="_blank">here</a>*/}
+        Updated 2025-04-08.
+      </p>
       <Row>
         <Col xs={12} md={4} className='sidebar'>
           <Row>
@@ -40,7 +43,7 @@ export default function Resources({loaderData}: Route.ComponentProps) {
 
 export function meta({data}: Route.MetaArgs) {
   return [
-    {title: `Machine Learning Guide Resources`},
-    {name: "description", content: "A syllabus of the best resources to learning machine learning from beginning ot end, with filters for selecting your preferred learning formats (video, audio, book) and difficulty (easy, medium, hard)"}
+    {title: `Learn Machine Learning & Artificial Intelligence`},
+    {name: "description", content: `${about} With filters for your preferred learning formats (video, audio, book) and difficulty (easy, medium, hard).`}
   ]
 }
