02ea7493b986a8a019bac4c2c92c7f4a4d002918
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-12 10:11:02 -0600

    mlg 28 transcript

--- reactrouter7/app/content/podcast/mlg/28/meta.js
+++ reactrouter7/app/content/podcast/mlg/28/meta.js
@@ -5,4 +5,4 @@
   export const guid = "8671d415236e9a9394a0c4aaa383e1ba"
   export const file = {}
   export const libsynEpisode = 6222761
-  export const teaser = 'Hyperparameters part 2: hyper-search, regularization, SGD optimizers, scaling'
+  export const teaser = 'The discussion continues on hyperparameters, touching on regularization techniques like dropout, L1 and L2, optimizers such as Adam, and feature scaling methods. The episode delves into hyperparameter optimization methods like grid search, random search, and Bayesian optimization, together with other aspects like initializers and scaling for neural networks.'
