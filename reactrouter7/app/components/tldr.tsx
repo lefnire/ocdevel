489094e0001f4e246b7c359ce253e1ea8ad1c8c8
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-15 10:52:53 -0600

    export resources.opml directly. Identical to before (I think just &amp; handled differently?), to prep for new resources

--- reactrouter7/app/components/tldr.tsx
+++ reactrouter7/app/components/tldr.tsx
@@ -3,4 +3,5 @@ import type {PropsWithChildren} from "react";
 
 export function TLDR({children}: PropsWithChildren) {
   return <Alert variant='info'><b>TL;DR</b> {children}</Alert>
+  // return <div className="text-bg-info p-3 rounded-2 my-2"><b>TL;DR</b> {children}</div>
 }
