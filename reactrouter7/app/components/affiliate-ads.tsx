cabb1b891a0952309ff9295c29ccc37536024c26
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-17 15:40:52 -0600

    remove battlestation from posts, add to sidebar

--- reactrouter7/app/components/affiliate-ads.tsx
+++ reactrouter7/app/components/affiliate-ads.tsx
@@ -8,6 +8,7 @@ import {clickAffiliate} from "~/components/analytics";
 import {memo, type ReactElement} from "react";
 import {FaComputerMouse} from "react-icons/fa6";
 import {FaLaptop} from "@react-icons/all-files/fa/FaLaptop";
+import {FaCode} from "@react-icons/all-files/fa/FaCode";
 
 const iconClass = {className: "me-2"}
 const sections = {
@@ -16,18 +17,20 @@ const sections = {
     header: <Link
       onClick={clickAffiliate('walk')}
       to="/walk"
-    >Walking desk</Link>,
+    >Walking Desk</Link>,
     body: "Stay healthy while I work & study"
   },
   descript: {
-    icon: <FaMicrophone {...iconClass} />,
-    header: <a
-      onClick={clickAffiliate('descript')}
-      href="/creator"
+    icon: <>
+      <FaCode {...iconClass} />
+      <FaMicrophone {...iconClass} />
+    </>,
+    header: <Link
+      onClick={clickAffiliate('software')}
+      to="/blog/20240111-tylers-setup"
       target="_blank"
-      rel="noopener noreferrer"
-    >Podcast Software</a>,
-    body: "AI audio/video editing",
+    >Software</Link>,
+    body: "AI code, AI content-creation",
   },
   ergo: {
     icon: <FaComputerMouse {...iconClass} />,
@@ -49,35 +52,35 @@ const sections = {
 
 const LinkCard = memo<{id:string}>(({id}) => {
   const section = sections[id]
-  return <Card className="border-0 bg-light h-100" body>
+  return <div className="h-100 m-1 p-1">
     <div>
       <small>
         {section.icon}
         {section.header}
       </small>
     </div>
-    <small>{section.body}</small>
-  </Card>
+    <small className="d-none d-md-block">{section.body}</small>
+  </div>
 })
 
 export function AffiliateAds({ className = "" }: { className?: string }) {
-  return <div className={className}>
+  return <Card className={`border-0 bg-light p-0 ${className}`} body>
     <h5 className="text-center">Tyler's Battlestation</h5>
     <Row>
-      <Col xs={12} md={6} className="mb-2 mb-md-0">
+      <Col xs={6} md={6}>
         <LinkCard id="walk" />
       </Col>
-      <Col xs={12} md={6}>
+      <Col xs={6} md={6}>
         <LinkCard id="descript" />
       </Col>
     </Row>
     <Row>
-      <Col xs={12} md={6} className="mb-2 mb-md-0">
+      <Col xs={6} md={6}>
         <LinkCard id="ergo" />
       </Col>
-      <Col xs={12} md={6}>
+      <Col xs={6} md={6}>
         <LinkCard id="laptop" />
       </Col>
     </Row>
-  </div>;
+  </Card>;
 }
--- reactrouter7/app/content/blog/20240111-tylers-setup/meta.js
+++ reactrouter7/app/content/blog/20240111-tylers-setup/meta.js
@@ -2,4 +2,4 @@ export const id = '20240111-tylers-setup'
 export const date = '2024-01-11'
 export const updated = "2025-04-17"
 export const title = "Tyler's Recommended Setup"
-export const teaser = "My recommended battlestation for work. Legion Pro for gaming and programming. A treadmill desk to improve focus and health. Ergonomic peripherals for preventing RSI. And software recommendations."
+export const teaser = "My recommended hardware and software. Legion Pro for gaming and programming. A walking desk to improve focus and health. Ergonomic mouse and keyboard to mitigate RSI. Code IDEs with AI agents. Podcast and YouTube recording software."
