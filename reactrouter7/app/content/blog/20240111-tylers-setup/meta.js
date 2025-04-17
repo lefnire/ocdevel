e74d027c609b973bd453a2d43793239cee98dcda
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-17 15:34:28 -0600

    update hardware/software pages

--- reactrouter7/app/content/blog/20240111-tylers-setup/meta.js
+++ reactrouter7/app/content/blog/20240111-tylers-setup/meta.js
@@ -1,4 +1,5 @@
 export const id = '20240111-tylers-setup'
 export const date = '2024-01-11'
+export const updated = "2025-04-17"
 export const title = "Tyler's Recommended Setup"
 export const teaser = "My recommended battlestation for work. Legion Pro for gaming and programming. A treadmill desk to improve focus and health. Ergonomic peripherals for preventing RSI. And software recommendations."
