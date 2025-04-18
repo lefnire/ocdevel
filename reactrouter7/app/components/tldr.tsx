1f317b0b13f1227e1b02e988814da4b57268f57f
Tyler Renelle <tylerrenelle@gmail.com>
2025-04-17 21:52:11 -0600

    TLDR cleanup

--- reactrouter7/app/components/tldr.tsx
+++ reactrouter7/app/components/tldr.tsx
@@ -1,7 +1,19 @@
 // import Alert from "react-bootstrap/cjs/Alert";
 import type {PropsWithChildren} from "react";
 
-export function TLDR({children}: PropsWithChildren) {
-  return <p className='ps-3 notice-card'><b>TL;DR</b> {children}</p>
+type TLDR = {
+  multiline?: boolean
+}
+export function TLDR({children, multiline}: PropsWithChildren<TLDR>) {
+  if (multiline) {
+    return <div className='tldr'>
+      <strong className="text-decoration-underline">TL;DR</strong>
+      <div>{children}</div>
+    </div>
+  }
+  return <div className='tldr'>
+    <strong className='text-decoration-underline me-2'>TL;DR</strong>
+    {children}
+  </div>
   // return <div className="text-bg-info p-3 rounded-2 my-2"><b>TL;DR</b> {children}</div>
 }
